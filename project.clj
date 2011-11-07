5211b1435babecf7056096aace8c14bd98c82c10
Daryl Dudey <daryljdudey@googlemail.com>
2011-11-07 20:51:12 +0000

    First commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject Ants "1.0.0"
+  :description "Google AI ants in Clojure"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/Ants/test/core.clj
@@ -0,0 +1,6 @@
+(ns Ants.test.core
+  (:use [Ants.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
