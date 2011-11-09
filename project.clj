d981e2fc59292695b5b1b7651fb80814c7a33439
Daryl dudey <daryljdudey@googlemail.com>
2011-11-09 22:56:33 +0000

    Still rubbish

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject Ants "1.0.0"
   :description "Google AI ants in Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :aot [ants MyBot])
--- src/MyBot.clj
+++ src/MyBot.clj
@@ -1,27 +1,58 @@
 (ns MyBot
-  (:use ants))
-;  (:import [java.util Random]))
+  (:use ants)
+  (:import [java.util Random]))
 
 (def directions [:north :east :west :south])
-;(def random (Random.))
+(def random (Random.))
+(def follow-mode (ref (hash-map)))
 
-(defn simple-bot [ant]
-  (let [dir (first (filter #(valid-move? ant %) directions))]
-    (when dir
-      (move ant dir))))
+(defn random-direction [ant]
+  (let [directions (filter #(valid-move? ant %) directions)
+        directions-count (count directions)
+        dir-index (. random nextInt directions-count)]
+    (let [dir (nth directions dir-index)]
+      (when dir
+        (.println *err* (str "Random: " dir))
+        (move ant dir)))))
+
+(defn next-dir [dir]
+  (cond
+   (= dir :north) :east
+   (= dir :east) :south
+   (= dir :south) :west
+   (= dir :west) :north))
+
+(defn follow-wall [ant dir]
+;  (loop
+;   (if (unoccupied? (move-ant ant dir
+  
+;  (.println *err* (str "Follow wall: "))
+  false)
+
+(defn follow-continue [ant]
+  (.println *err* (str "Continue follow wall: ")))
 
 (defn bot-food-gatherer [ant]
   (let [food-distance (sort-by second (map #(list % (distance ant %)) (food)))
         closest-food (first (first food-distance))]
 
-    ;; Any visible food?
-    (when-not (nil? closest-food)
-      (let [directions (direction ant closest-food)
-            dir (first (filter #(valid-move? ant %) directions))]
-        (when dir
-          (.println *err* (str ant " " dir " " closest-food))
-          (move ant dir))))))
-
+    (cond
+     
+     (contains? follow-mode ant) (follow-continue ant)
+     
+     ;; Any visible food?
+     (not (nil? closest-food)) (let [food-directions (direction ant closest-food)
+                                     dir (first (filter #(valid-move? ant %) food-directions))]
+                                 (if dir
+                                   (do
+                                     (.println *err* (str "Food: " dir))
+                                     (move ant dir))
+                                   (when-not (follow-wall ant dir)
+                                     (random-direction ant))))
+     
+     ;; Random move
+     :else (random-direction ant))))
+      
 (defn ant-loop []
   (doseq [ant (my-ants)]
     (bot-food-gatherer ant)))
