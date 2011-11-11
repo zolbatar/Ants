401743d4f51b8bf8d1e15ab11b61d6f79e48af95
Daryl dudey <daryljdudey@googlemail.com>
2011-11-11 21:29:44 +0000

    Single node scan

--- Clojure/src/nodes.clj
+++ Clojure/src/nodes.clj
@@ -1,40 +1,27 @@
 (ns nodes
   (:use ants))
 
-(def nodes (atom (list {})))
-
-(defn add-node [loc dir distance]
-  (swap! nodes conj {:loc loc :dir dir :distance distance}))
-
-(defn does-node-exist? [loc dir]
-  (= (count (filter #(and (= (:loc %) loc) (= (:dir %) dir)) @nodes)) 0))
+(defn add-node [node dir distance]
+  (swap! node assoc dir distance))
 
 (defn scan-node-dir [loc dir]
   (loop [distance 0 pos loc]
     (let [new-pos (valid-move? pos dir)]
       (if new-pos
         (recur (inc distance) new-pos)
-        (do
-          (add-node loc dir distance)
-          pos)))))
+        distance))))
 
-(defn scan-node [depth loc dir]
-  (.println *err* (str depth " " loc " " dir))
-  (when (does-node-exist? loc dir)
-    (let [next-loc (scan-node-dir loc dir)]
-      (when-not (or (= depth 0) (= next-loc loc))
-        (scan-node (inc depth) next-loc :north)
-        (scan-node (inc depth) next-loc :east)
-        (scan-node (inc depth) next-loc :south)
-        (scan-node (inc depth) next-loc :west)))))
+(defn scan-node [node loc]
+  (add-node node :north (scan-node-dir loc :north))
+  (add-node node :east (scan-node-dir loc :east))
+  (add-node node :south (scan-node-dir loc :south))
+  (add-node node :west (scan-node-dir loc :west)))
 
 (defn map-out-map []
-  (doseq [x (hills)]
-    (scan-node 0 x :north)
-    (scan-node 0 x :east)
-    (scan-node 0 x :south)
-    (scan-node 0 x :west))
-  (.println *err* @nodes))
+  (let [node (atom {})] 
+    (doseq [x (hills)]
+      (scan-node node x))
+    (.println *err* @node)))
     
 
 
